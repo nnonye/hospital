a12ccbe278f1360f86391a9f2470a6759245771d
Theophilus Glory <theophilusglow@gmail.com>
2022-03-15 17:02:32 +0000

    init commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-page class="flex column">
+
+
+
+    <div class="col text-white text-center">
+      <div class="tobi q-pb-md text-h2 text-weight-light">
+        Hospitals
+      </div>
+      <div class="q-pt-md text-h6 text-weight-light">
+        Closest to me
+      </div>
+    </div>
+        <q-btn to="/search" class="glow q-pb-md text-h6 text-weight-bold"
+         color="rgb(76, 129, 146), rgb(192, 216, 215)" label="Search" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default  {
+  name: 'PageIndex',
+
+}
+</script>
+
+<style scoped>
+  *{
+    background-image: linear-gradient(to right, rgb(76, 129, 146), rgb(192, 216, 215));
+  }
+  .glow{
+    position: absolute;
+    bottom: 65%;
+    left: 42%;
+  }
+  .tobi {
+    padding-top: 30px;
+  }
+</style>
---
+++ src/pages/search.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-page class="">
+    <div class="row q-col-gutter-sm q-pr" >
+      <!-- <q-btn color="indigo" label="Button" v-for="n in 7" :key="`xl-${n}`" /> -->
+      <q-card
+      class="my-card text-white row-5"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+      <q-card-section class="">
+        <div class="text-h6">Hospital Name</div>
+        <div class="text-subtitle2">L.G.A</div>
+      </q-card-section>
+
+      <q-card-section class="">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+      <q-card
+      class="my-card text-white row-5"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+      <q-card-section >
+        <div class="text-h6">Hospital Name</div>
+        <div class="text-subtitle2">L.G.A</div>
+      </q-card-section>
+
+      <q-card-section class="">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+      <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+      <q-card-section class="col-5">
+        <div class="text-h6">Hospital Name</div>
+        <div class="text-subtitle2">L.G.A</div>
+      </q-card-section>
+
+      <q-card-section class="">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+      <q-card
+      class="my-card text-white"
+      style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+      <q-card-section class="col-5">
+        <div class="text-h6">Hospital Name</div>
+        <div class="text-subtitle2">L.G.A</div>
+      </q-card-section>
+
+      <q-card-section class="">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+    </div>
+    </q-page>
+
+</template>
+
+  <script>
+export default {
+  setup () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
+}
+</script>
+
+<style  scoped>
+.my-card{
+   width: 100%;
+  max-width: 250px;
+  height: 20%;
+}
+
+</style>
